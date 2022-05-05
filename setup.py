a49bd77fd51913c12185d03ec34a8bbed2320504
HFrost0 <2498393441@qq.com>
2022-05-05 22:58:59 +0800

    🔧新的命令行

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_license():
 setup(
     name='bilix',
     python_requires=">=3.8",
-    version='0.3.3.6',
+    version='0.3.4',
     author='HFrost0',
     author_email='hhlfrost@gmail.com',
     description='⚡️快如闪电的b站视频下载工具，基于Python现代Async异步特性，高速批量下载整部动漫，电视剧，电影，up投稿等',
@@ -23,12 +23,16 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(),
     entry_points={
-        "console_scripts": "bilix=bilix._cli:main"
+        "console_scripts": ["bilix=bilix._cli:main",
+                            "bilix_old=bilix._cli_old:main"]
+
     },
     install_requires=[
         "httpx[http2]",
+        'anyio',
         'rich',
         'json5',
         'protobuf',
+        'click'
     ],
 )
