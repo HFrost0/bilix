76eb08eed60edbe8b3ba09bf6b67c9ec492f95c2
HFrost0 <2498393441@qq.com>
2022-05-20 13:09:10 +0800

    remove version to `__init__.py`

--- bilix/__init__.py
+++ bilix/__init__.py
@@ -1,1 +1,3 @@
 from .download import Downloader
+
+__version__ = "0.6.9"
--- setup.py
+++ setup.py
@@ -7,10 +7,19 @@ def get_long_description():
         return f.read()
 
 
+def get_version() -> str:
+    with open('bilix/__init__.py', 'r', encoding='utf8') as f:
+        for line in f.readlines():
+            if line.startswith("__version__"):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name='bilix',
     python_requires=">=3.8",
-    version='0.6.8',
+    version=get_version(),
     author='HFrost0',
     author_email='hhlfrost@gmail.com',
     description='⚡️快如闪电的b站视频下载工具，基于Python现代Async异步特性，高速批量下载整部动漫，电视剧，电影，up投稿等',
