f653370373e9c68d2466a9aa5faf2a103e4a1631
HFrost0 <2498393441@qq.com>
2022-08-20 13:06:25 +0800

    add yinghuacd support

--- bilix/download/downloader_yinghuacd.py
+++ bilix/download/downloader_yinghuacd.py
@@ -2,49 +2,39 @@ import asyncio
 import httpx
 from typing import Sequence
 
-import bilix.api.yhdmp as api
+import bilix.api.yinghuacd as api
 from bilix.assign import Handler
+from bilix.log import logger
 from bilix.utils import legal_title, cors_slice
 from bilix.download.base_downloader_m3u8 import BaseDownLoaderM3u8
 
 
-class DownloaderYhdmp(BaseDownLoaderM3u8):
+class DownloaderYinghuacd(BaseDownLoaderM3u8):
     def __init__(self, videos_dir: str = "videos", video_concurrency: int = 3, part_concurrency: int = 10):
         client = httpx.AsyncClient(
-            headers={'user-agent': 'PostmanRuntime/7.29.0', "Referer": "https://www.yhdmp.cc"}, http2=True)
-        super(DownloaderYhdmp, self).__init__(client, videos_dir, video_concurrency, part_concurrency)
+            headers={'user-agent': 'PostmanRuntime/7.29.0', "Referer": "http://www.yinghuacd.com"}, http2=False)
+        super(DownloaderYinghuacd, self).__init__(client, videos_dir, video_concurrency, part_concurrency)
 
     async def get_series(self, url: str, p_range: Sequence[int] = None, hierarchy=True):
         video_info = await api.get_video_info(url)
-        ep_idx = video_info.ep_idx
-        play_idx = video_info.play_idx
-        title = video_info.title
         if hierarchy:
             hierarchy = self._make_hierarchy_dir(hierarchy, video_info.title)
-
-        # no need to reuse get_video since we only need m3u8_url
-        async def get_video(name):
-            m3u8_url = await api.get_m3u8_url(url)
-            await self.get_m3u8_video(m3u8_url=m3u8_url, name=name, hierarchy=hierarchy if hierarchy else '')
-
-        cors = []
-        for idx, (sub_title, url) in enumerate(video_info.play_info[play_idx]):
-            if ep_idx == idx:
-                cors.append(self.get_m3u8_video(m3u8_url=video_info.m3u8_url, name=legal_title(title, sub_title),
-                                                hierarchy=hierarchy if hierarchy else ''))
-            else:
-                cors.append(get_video(legal_title(title, sub_title)))
+        cors = [self.get_video(u, hierarchy=hierarchy if hierarchy else '', extra=video_info if u == url else None)
+                for _, u in video_info.play_info]
         if p_range:
             cors = cors_slice(cors, p_range)
         await asyncio.gather(*cors)
 
-    async def get_video(self, url: str, hierarchy: str = ''):
-        video_info = await api.get_video_info(url, self.client)
+    async def get_video(self, url: str, hierarchy: str = '', extra=None):
+        if extra is None:
+            video_info = await api.get_video_info(url, self.client)
+        else:
+            video_info = extra
         name = legal_title(video_info.title, video_info.sub_title)
         await self.get_m3u8_video(m3u8_url=video_info.m3u8_url, name=name, hierarchy=hierarchy)
 
 
-@Handler(name='樱花动漫P')
+@Handler(name='樱花动漫')
 def handle(
         method: str,
         key: str,
@@ -65,9 +55,9 @@ def handle(
         only_audio: bool,
         p_range,
 ):
-    if 'yhdmp' in key:
-        d = DownloaderYhdmp(videos_dir=videos_dir, video_concurrency=video_concurrency,
-                            part_concurrency=part_concurrency)
+    if 'yinghuacd' in key:
+        d = DownloaderYinghuacd(videos_dir=videos_dir, video_concurrency=video_concurrency,
+                                part_concurrency=part_concurrency)
         if method == 'get_series' or method == 's':
             cor = d.get_series(key, p_range=p_range, hierarchy=hierarchy)
             return d, cor
@@ -75,3 +65,13 @@ def handle(
             cor = d.get_video(key)
             return d, cor
         raise ValueError(f'For {d.__class__.__name__} "{method}" is not available')
+
+
+if __name__ == '__main__':
+    async def main():
+        async with DownloaderYinghuacd() as d:
+            await d.get_series("http://www.yinghuacd.com/v/5606-7.html")
+
+
+    logger.setLevel('DEBUG')
+    asyncio.run(main())
