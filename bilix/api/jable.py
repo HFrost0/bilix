9bd7bed2774edfe6b342645a103ceffa4d1e0851
showthesunli <showthesunli@gmail.com>
2022-11-11 03:31:33 +0800

    fix issue #45

--- bilix/api/jable.py
+++ bilix/api/jable.py
@@ -7,7 +7,7 @@ from bilix.log import logger
 from bilix.utils import legal_title, req_retry
 
 BASE_URL = "https://jable.tv"
-_dft_headers = {'user-agent': 'PostmanRuntime/7.29.0', "Referer": BASE_URL}
+_dft_headers = {"user-agent": "PostmanRuntime/7.29.0", "Referer": BASE_URL}
 
 
 @dataclass
@@ -21,31 +21,43 @@ class VideoInfo:
 
 
 async def get_video_info(client: httpx.AsyncClient, url_or_avid: str) -> VideoInfo:
-    if url_or_avid.startswith('http'):
+    if url_or_avid.startswith("http"):
         url = url_or_avid
-        avid = url.split('/')[-2]
+        avid = url.split("/")[-2]
     else:
-        url = f'{BASE_URL}/videos/{url_or_avid}/'
+        url = f"{BASE_URL}/videos/{url_or_avid}/"
         avid = url_or_avid
     avid = avid.upper()
     res = await req_retry(client, url)  # proxies default global in httpx
     soup = BeautifulSoup(res.text, "html.parser")
-    title = soup.find('meta', property="og:title")['content']
+    title = soup.find("meta", property="og:title")["content"]
     title = legal_title(title)
-    model_name = soup.find("span", class_="placeholder rounded-circle")['title']
-    img_url = soup.find('meta', property="og:image")['content']
-    m3u8_url = re.findall(r'http.*m3u8', res.text)[0]
-    video_info = VideoInfo(url=url, avid=avid, title=title, img_url=img_url, m3u8_url=m3u8_url, model_name=model_name)
+    try:
+        model_name = soup.find("span", class_="placeholder rounded-circle")["title"]
+    except:
+        model_name = soup.find("img", class_="avatar rounded-circle")["title"]
+    finally:
+        model_name = ""
+    img_url = soup.find("meta", property="og:image")["content"]
+    m3u8_url = re.findall(r"http.*m3u8", res.text)[0]
+    video_info = VideoInfo(
+        url=url,
+        avid=avid,
+        title=title,
+        img_url=img_url,
+        m3u8_url=m3u8_url,
+        model_name=model_name,
+    )
     return video_info
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     async def main():
         _dft_client = httpx.AsyncClient(headers=_dft_headers, http2=True)
         return await asyncio.gather(
             get_video_info(_dft_client, "MIAA-650"),
         )
-
 
     logger.setLevel("DEBUG")
     result = asyncio.run(main())
