f07f8ca7effb39f8d5c5a3b444289d5ac68bfacb
HFrost0 <2498393441@qq.com>
2022-11-11 10:24:47 +0800

    fix model_name find problem

--- tests/api/test_jable_api.py
+++ tests/api/test_jable_api.py
@@ -8,7 +8,7 @@ client = httpx.AsyncClient(headers={'user-agent': 'PostmanRuntime/7.29.0', 'refe
 
 @pytest.mark.asyncio
 async def test_get_video_info():
-    data = await api.get_video_info(client, "https://jable.tv/videos/ssis-448/")
+    data = await api.get_video_info(client, "https://jable.tv/videos/ssis-533/")
     assert data.model_name
-    data = await api.get_video_info(client, "https://jabel.tv/videos/ssis-533/")
+    data = await api.get_video_info(client, "https://jable.tv/videos/ssis-448/")
     assert data.model_name
