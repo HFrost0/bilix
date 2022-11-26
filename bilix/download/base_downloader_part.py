3386f2dfe6aaa9d85b8550a09013aeebc21c2455
HFrost0 <2498393441@qq.com>
2022-11-26 17:57:20 +0800

    improve BaseDownloaderPart redirect

--- bilix/download/base_downloader_part.py
+++ bilix/download/base_downloader_part.py
@@ -1,6 +1,6 @@
 import asyncio
 import re
-from typing import Union, Sequence
+from typing import Union, List, Iterable
 import aiofiles
 import httpx
 import random
@@ -27,15 +27,17 @@ class BaseDownloaderPart(BaseDownloader):
         super(BaseDownloaderPart, self).__init__(client, videos_dir, speed_limit=speed_limit, progress=progress)
         self.part_concurrency = part_concurrency
 
-    async def _content_length(self, url_or_urls: Union[str, Sequence[str]]) -> int:
-        # deal with HEAD 404 bug https://github.com/HFrost0/bilix/issues/16
-        # and redirect like Douyin, TikTok
-        res = await req_retry(self.client, url_or_urls, follow_redirects=True, headers={'Range': 'bytes=0-1'})
+    async def _content_length(self, urls: List[Union[str, httpx.URL]]) -> int:
+        # use GET instead of HEAD due to 404 bug https://github.com/HFrost0/bilix/issues/16
+        res = await req_retry(self.client, urls[0], follow_redirects=True, headers={'Range': 'bytes=0-1'})
         total = int(res.headers['Content-Range'].split('/')[-1])
+        # change origin url to redirected position to avoid twice redirect
+        if res.history:
+            urls[0] = res.url
         return total
 
-    async def get_media(self, url_or_urls: Union[str, Sequence[str]],
-                        media_name, task_id=None, hierarchy: str = '', retry: int = 5) -> str:
+    async def get_media(self, url_or_urls: Union[str, Iterable[str]],
+                        media_name: str, task_id=None, hierarchy: str = '', retry: int = 5) -> str:
         """
 
         :param url_or_urls: media url or urls with backups
@@ -47,10 +49,11 @@ class BaseDownloaderPart(BaseDownloader):
         """
         file_dir = f'{self.videos_dir}/{hierarchy}' if hierarchy else self.videos_dir
         file_path = f'{file_dir}/{media_name}'
+        urls = [url_or_urls] if isinstance(url_or_urls, str) else [url for url in url_or_urls]
         if os.path.exists(file_path):
             logger.info(f'[green]已存在[/green] {media_name}')
             return file_path
-        total = await self._content_length(url_or_urls)
+        total = await self._content_length(urls)
         if task_id is not None:
             await self.progress.update(task_id, total=self.progress.tasks[task_id].total + total, visible=True)
         else:
@@ -63,7 +66,7 @@ class BaseDownloaderPart(BaseDownloader):
             end = (i + 1) * part_length - 1 if i < self.part_concurrency - 1 else total - 1
             part_name = f'{media_name}-{start}-{end}'
             part_names.append(part_name)
-            cors.append(self._get_media_part(url_or_urls, part_name, task_id, hierarchy=hierarchy, retry=retry))
+            cors.append(self._get_media_part(urls, part_name, task_id, hierarchy=hierarchy, retry=retry))
         file_list = await asyncio.gather(*cors)
         await merge_files(file_list, new_name=file_path)
         if self.progress.tasks[task_id].finished:
@@ -71,7 +74,7 @@ class BaseDownloaderPart(BaseDownloader):
             logger.info(f"[cyan]已完成[/cyan] {media_name}")
         return file_path
 
-    async def _get_media_part(self, url_or_urls: Union[str, Sequence[str]],
+    async def _get_media_part(self, urls: List[Union[str, httpx.URL]],
                               part_name, task_id, exception=0, hierarchy: str = '', retry: int = 5):
         file_dir = f'{self.videos_dir}/{hierarchy}' if hierarchy else self.videos_dir
         if exception > retry:
@@ -86,11 +89,13 @@ class BaseDownloaderPart(BaseDownloader):
                 await self.progress.update(task_id, advance=downloaded)
         if start > end:
             return file_path  # skip already finished
-        url = url_or_urls if type(url_or_urls) is str else random.choice(url_or_urls)
+        url_idx = random.randint(0, len(urls) - 1)
         try:
-            async with self.client.stream("GET", url, follow_redirects=True,
+            async with self.client.stream("GET", urls[url_idx], follow_redirects=True,
                                           headers={'Range': f'bytes={start}-{end}'}) as r:
                 r.raise_for_status()
+                if r.history:  # avoid twice redirect
+                    urls[url_idx] = r.url
                 async with aiofiles.open(file_path, 'ab') as f:
                     async for chunk in r.aiter_bytes():
                         await f.write(chunk)
@@ -100,16 +105,16 @@ class BaseDownloaderPart(BaseDownloader):
                 logger.warning(
                     f'STREAM {e.__class__.__name__} 异常可能由于网络条件不佳或并发数过大导致，若重复出现请考虑降低并发数')
             await asyncio.sleep(.1 * exception)
-            await self._get_media_part(url_or_urls, part_name, task_id, exception=exception + 1, hierarchy=hierarchy)
+            await self._get_media_part(urls, part_name, task_id, exception=exception + 1, hierarchy=hierarchy)
         except httpx.HTTPStatusError as e:
             logger.warning(f"STREAM {e}")
-            await asyncio.sleep(.5)
-            await self._get_media_part(url_or_urls, part_name, task_id, exception=exception + 1, hierarchy=hierarchy)
+            await asyncio.sleep(.5 * (exception + 1))
+            await self._get_media_part(urls, part_name, task_id, exception=exception + 1, hierarchy=hierarchy)
         except Exception as e:
             if exception > 1:
                 logger.warning(f'STREAM {e.__class__.__name__} 未知异常')
             await asyncio.sleep(.5 * exception)
-            await self._get_media_part(url_or_urls, part_name, task_id, exception=exception + 1, hierarchy=hierarchy)
+            await self._get_media_part(urls, part_name, task_id, exception=exception + 1, hierarchy=hierarchy)
         return file_path
 
 
