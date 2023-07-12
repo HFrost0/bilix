0c13cece11ce0dfebf1a14279eb70a32d5855e2f
HFrost0 <hhlfrost@gmail.com>
2023-07-12 23:23:05 +0800

    bilibili api update

--- bilix/sites/bilibili/utils.py
+++ bilix/sites/bilibili/utils.py
@@ -2,36 +2,18 @@ import re
 
 
 def parse_ids_from_url(url_or_string: str):
-    """
-    aid, bvid, page_num = parse_ids_from_url(url_or_string)
-
-    例如：
-    ```
-    [0] https://www.bilibili.com/video/av170001
-    > aid = 170001, bvid = None, page_num = None
-    [1] http://www.bilibili.com/video/BV1Xx41117Tz/?ba=labala&p=3#time=1234
-    > aid = None, bvid = "BV1Xx41117Tz", page_num = 3
-    [2] av170001
-    > aid = 170001, bvid = None, page_num = None
-    [3] BV1sE411w7tQ?p=2
-    > aid = None, bvid = "BV1sE411w7tQ", page_num = 2
-    ```
-    """
-
-    bvid = aid = page_num = None
+    bvid, aid, page_num = None, None, 1
     if re.match(r'https?://www.bilibili.com/video/BV\w+', url_or_string) or re.match(r'BV\w+', url_or_string):
         bvid = re.search(r'(BV\w+)', url_or_string).groups()[0]
         assert bvid.isalnum()
-        assert isinstance(bvid, str)
     elif re.match(r'https?://www.bilibili.com/video/av\d+', url_or_string) or re.match(r'av\d+', url_or_string):
         aid = re.search(r'av(\d+)', url_or_string).groups()[0]
         assert aid.isdigit()
         aid = int(aid)
-
+    else:
+        raise ValueError(f"{url_or_string} is not a valid bilibili video url")
     # ?p=123 or &p=123
-    if re.match(r'.*[?&]p=(\d+)', url_or_string):
-        page_num = int(re.search(r'.*[?&]p=(\d+)', url_or_string).groups()[0])
-
-    assert bvid or aid, "url is not a valid bilibili video url"
-
+    if m := re.match(r'.*[?&]p=(\d+)', url_or_string):
+        page_num = int(m.groups()[0])
+        assert page_num > 1
     return aid, bvid, page_num
--- bilix/sites/bilibili/utils_test.py
+++ bilix/sites/bilibili/utils_test.py
@@ -1,6 +1,5 @@
-import pytest
-
 from bilix.sites.bilibili.utils import parse_ids_from_url
+
 
 def test_parse_ids_from_url():
     strings = [
@@ -8,12 +7,12 @@ def test_parse_ids_from_url():
         "http://www.bilibili.com/video/BV1Xx41117Tz/?ba=labala&p=3#time=1234",
         "av170001",
         "BV1sE411w7tQ?p=2&from=search",
-        ]
+    ]
     results = [
-        (170001, None, None),
+        (170001, None, 1),
         (None, 'BV1Xx41117Tz', 3),
-        (170001, None, None),
+        (170001, None, 1),
         (None, 'BV1sE411w7tQ', 2),
-        ]
+    ]
     for index, string in enumerate(strings):
         assert parse_ids_from_url(string) == results[index]
